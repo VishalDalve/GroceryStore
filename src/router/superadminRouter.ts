9907800b903fdc492c6237fc6e24d153f7062fca
vishaldalve <vishal.dalve@mail.vinove.com>
2019-11-24 15:16:38 +0530

    Front Added

--- src/router/superadminRouter.ts
+++ src/router/superadminRouter.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 
 import SuperAdminController from '../controllers/superAdminController';
 import { Auth } from '../config/authMiddleware';
-import {upload} from '../service/fileUpload';
+import { upload } from '../service/fileUpload';
 
 /**
  * @class UserRouter
@@ -21,17 +21,17 @@ export default class SuperAdminRouter {
         this.router.get('/getInactiveUsers', Auth, SuperAdminController.getInactiveUsers);
         this.router.put('/deleteUser', Auth, SuperAdminController.deleteUser);
         this.router.put('/updateUser', Auth, SuperAdminController.updateUser);
-        this.router.post('/createUser',Auth, SuperAdminController.createUser);
+        this.router.post('/createUser', Auth, SuperAdminController.createUser);
 
         // Product management -----------
-        this.router.post('/createProduct',[Auth, upload.single('file')], SuperAdminController.createProduct);
+        this.router.post('/createProduct', [Auth, upload.single('file')], SuperAdminController.createProduct);
         this.router.get('/getProduct', SuperAdminController.getProduct);
         this.router.put('/updateProduct', Auth, SuperAdminController.updatProduct);
-        
-         // Category management -----------
-         this.router.post('/createCategory', Auth, SuperAdminController.createCategory);
-         this.router.get('/getCategory', SuperAdminController.getCategory);
-         this.router.put('/updateCategory', Auth, SuperAdminController.updatCategory);
+
+        // Category management -----------
+        this.router.post('/createCategory', Auth, SuperAdminController.createCategory);
+        this.router.get('/getCategory', SuperAdminController.getCategory);
+        this.router.put('/updateCategory', Auth, SuperAdminController.updatCategory);
 
     }
 }
